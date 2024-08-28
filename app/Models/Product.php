64e9ed5e403cbde190de99d7f254a42804cc5689
LAPTOP-VN9H03PP\xhaw0 <ion3256382@gmail.com>
2024-08-28 19:21:01 +0800

    model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Tags\Tag;
 
 class Product extends Model
 {
     use HasFactory;
+    public function tags()
+    {
+        return $this->morphToMany(Tag::class, 'taggable');
+    }
+
+    public function media()
+    {
+        return $this->morphMany(Media::class, 'model');
+    }
 }
