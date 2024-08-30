96c119833115d93495cbd8731932acd32b8bb733
yao <kinyao1025@gmail.com>
2024-08-30 23:53:15 +0800

    前端:第四次之2

--- resources/views/Product_create.blade.php
+++ resources/views/Product_create.blade.php
@@ -40,6 +40,7 @@
                         </li>
                         <li class="font-semibold text-gray-900 hover:text-gray-400 transition ease-in-out duration-300 mb-5 lg:mb-0 text-2xl">
                             <a href="/user_product_create">刊登</a>
+                            
                         </li>
                         <li class="font-semibold text-gray-900 hover:text-gray-400 transition ease-in-out duration-300 mb-5 lg:mb-0 text-2xl">
                             <a href="/user_product_check">我的商品</a>
@@ -143,11 +144,10 @@
         </div>
       </div>
       <button
-        class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-11 rounded-md px-8"
-        type="submit"
-      >
+        class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-blue-700 h-11 rounded-md px-8"type="submit">
         刊登商品
       </button>
+      
     </form>
     <!-- <div class="grid gap-6 md:grid-cols-2 items-start">
       <div>
