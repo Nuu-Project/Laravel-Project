78d448147e2d019f2efbbf31f51f1e0a68720eed
LAPTOP-VN9H03PP\xhaw0 <ion3256382@gmail.com>
2024-09-06 20:34:37 +0800

    email 要求

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,7 +31,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class, 'regex:/^[a-zA-Z0-9._%+-]+@o365\.nuu\.edu\.tw$/'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
