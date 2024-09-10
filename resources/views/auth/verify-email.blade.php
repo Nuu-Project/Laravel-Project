20cb12e2d5da21189deef9d178a9817ef16b3a7f
LAPTOP-VN9H03PP\xhaw0 <ion3256382@gmail.com>
2024-09-10 17:53:05 +0800

    註冊後頁面變更

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,11 +1,23 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('成功傳送驗證信件。') }}
+        {{ __('感謝您的註冊！在開始之前，請通過點擊我們剛剛發送給您的電子郵件中的鏈接來驗證您的電子郵件地址。如果您沒有收到電子郵件，我們將很樂意再發送一封。') }}
     </div>
 
-    <div class="flex items-center justify-end mt-4">
-        <a class="no-underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('dashboard') }}">
-            {{ __('點擊驗證信跳轉回首頁') }}
-        </a>
+    @if (session('status') == 'verification-link-sent')
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ __('新的驗證連結已傳送至郵箱') }}
+        </div>
+    @endif
+
+    <div class="mt-4 flex items-center justify-between">
+        <form method="POST" action="{{ route('email/verification-notification') }}">
+            @csrf
+
+            <div>
+                <x-primary-button>
+                    {{ __('重新寄一封驗證信') }}
+                </x-primary-button>
+            </div>
+        </form>
     </div>
 </x-guest-layout>
