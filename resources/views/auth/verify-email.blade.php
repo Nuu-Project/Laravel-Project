dd694ce85e8f3edb28bee8e431e6046a1c01986b
LAPTOP-VN9H03PP\xhaw0 <ion3256382@gmail.com>
2024-09-23 10:40:00 +0800

    註冊驗證修復

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -3,6 +3,7 @@
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
+    @if (session('status') == 'verification-link-sent')
     <div class="flex items-center justify-end mt-4">
         <a class="no-underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('dashboard') }}">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
@@ -11,7 +12,7 @@
     @endif
 
     <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('email/verification-notification') }}">
+        <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
             <div>
