8c374064839c5bf24b2c2346cede17fc6edf7139
yancheng <dppss99015@gmail.com>
2024-10-22 23:35:02 +0800

    修正軟刪除標籤顯示問題

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -13,15 +13,19 @@ class ProductController extends Controller
     {
         // 獲取請求中的標籤 slug
         $tagSlugs = $request->input('tags', []);
-        // 新增：獲取搜索關鍵字
+        // 獲取搜索關鍵字
         $search = $request->input('search');
-        // 將 slug 轉換為標籤名稱和類型
-        $tags = Tag::whereIn('slug->zh', $tagSlugs)->get()->map(function ($tag) {
-            return [
-                'name' => $tag->getTranslation('name', 'zh'),
-                'type' => $tag->type,
-            ];
-        })->toArray();
+        
+        // 修改：只獲取未被軟刪除的標籤
+        $tags = Tag::whereIn('slug->zh', $tagSlugs)
+            ->whereNull('deleted_at')
+            ->get()
+            ->map(function ($tag) {
+                return [
+                    'name' => $tag->getTranslation('name', 'zh'),
+                    'type' => $tag->type,
+                ];
+            })->toArray();
 
         // 根據所有標籤篩選商品
         $productsQuery = Product::with(['media', 'user'])
@@ -31,18 +35,21 @@ class ProductController extends Controller
             foreach ($tags as $tag) {
                 $productsQuery->whereHas('tags', function ($query) use ($tag) {
                     $query->where('name->zh', $tag['name'])
-                          ->where('type', $tag['type']);
+                          ->where('type', $tag['type'])
+                          ->whereNull('deleted_at'); // 確保只使用未被軟刪除的標籤
                 });
             }
         }
 
-         // 新增：如果有搜索關鍵字，則加入搜索條件
+        // 如果有搜索關鍵字，則加入搜索條件
         if ($search) {
             $productsQuery->where('name', 'like', '%' . $search . '%');
         }
 
         $products = $productsQuery->paginate(6);
-        $allTags = Tag::all();
+        
+        // 修改：只獲取未被軟刪除的所有標籤
+        $allTags = Tag::whereNull('deleted_at')->get();
 
         return view('guest.Product', compact('products', 'allTags', 'tagSlugs', 'search'));
     }
