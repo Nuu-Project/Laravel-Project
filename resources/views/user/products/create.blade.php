5545028a93d0fb03e63bee944f1eb8a13a57f13b
yancheng <dppss99015@gmail.com>
2024-10-24 09:38:56 +0800

    修正顯示問題

--- resources/views/user/products/create.blade.php
+++ resources/views/user/products/create.blade.php
@@ -148,7 +148,7 @@
                                         <div class="relative w-full aspect-square">
                                             <input type="file" name="images[]" id="image{{ $i }}" class="hidden" accept="image/*" onchange="previewImage(this, {{ $i }})">
                                             <label for="image{{ $i }}" class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 overflow-hidden">
-                                                <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                                <div id="placeholder{{ $i }}" class="flex flex-col items-center justify-center pt-5 pb-6">
                                                     <svg class="w-8 h-8 mb-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                                                     </svg>
@@ -176,7 +176,7 @@
     <script>
     function previewImage(input, number) {
         const preview = document.getElementById('preview' + number);
-        const label = preview.parentElement;
+        const placeholder = document.getElementById('placeholder' + number);
         const deleteButton = document.getElementById('deleteButton' + number);
         const file = input.files[0];
         const reader = new FileReader();
@@ -184,7 +184,7 @@
         reader.onloadend = function () {
             preview.src = reader.result;
             preview.classList.remove('hidden');
-            label.querySelector('div').classList.add('hidden');
+            placeholder.classList.add('hidden');
             deleteButton.classList.remove('hidden');
         }
 
@@ -193,7 +193,7 @@
         } else {
             preview.src = "";
             preview.classList.add('hidden');
-            label.querySelector('div').classList.remove('hidden');
+            placeholder.classList.remove('hidden');
             deleteButton.classList.add('hidden');
         }
     }
@@ -201,13 +201,13 @@
     function removeImage(number) {
         const input = document.getElementById('image' + number);
         const preview = document.getElementById('preview' + number);
-        const label = preview.parentElement;
+        const placeholder = document.getElementById('placeholder' + number);
         const deleteButton = document.getElementById('deleteButton' + number);
 
         input.value = '';
         preview.src = "";
         preview.classList.add('hidden');
-        label.querySelector('div').classList.remove('hidden');
+        placeholder.classList.remove('hidden');
         deleteButton.classList.add('hidden');
     }
 
