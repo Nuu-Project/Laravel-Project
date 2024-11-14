a95c3729c30df9cc4583c315b4529d30489c4f5f
LAPTOP-VN9H03PP\xhaw0 <ion3256382@gmail.com>
2024-11-14 14:51:46 +0800

    衝突修改2

--- routes/admin.php
+++ routes/admin.php
@@ -14,23 +14,23 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->grou
     Route::get('/products', [ManageableProductsController::class, 'index'])
         ->name('products.index');
     // 商品管理下架 未返回畫面
-    Route::put('/products/{product}/demote', [DownShelvesController::class, 'demoteData'])
-        ->name('DownShelvesController.demote');
+    Route::put('/products/{product}', [DownShelvesController::class, 'demoteData'])
+        ->name('products.demote');
 
     // 用戶管理頁
     Route::get('/user', [UserController::class, 'index'])
-        ->name('user.index');
+        ->name('users.index');
     // 用戶停用
     Route::post('/user/suspend', [UserController::class, 'suspend'])
-        ->name('user.suspend');
+        ->name('users.suspend');
 
     // 留言管理頁 controller要改
     Route::get('/message', [ChirpController::class, 'adminMessage'])
-        ->name('message.index');
+        ->name('messages.index');
 
     // 角色管理路由
     Route::get('/roles', [RoleController::class, 'index'])
-        ->name('role.index');
+        ->name('roles.index');
     // ??
     Route::post('/roles', [RoleController::class, 'store'])
         ->name('roles.store');
@@ -40,7 +40,7 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->grou
     Route::put('/roles/{role}', [RoleController::class, 'update'])
         ->name('roles.update');
     // 標籤 頁面,新增,修改,刪除
-    Route::resource('tags', TagController::class)
+    Route::resource('/tags', TagController::class)
         ->except(['show'])
         ->withTrashed();
     // 標籤 啟用
@@ -49,6 +49,6 @@ Route::prefix('admin')->name('admin.')->middleware(['auth', 'role:admin'])->grou
         ->withTrashed();
 
     // 檢舉詳情頁
-    Route::get('/report', [ReportDetailController::class, 'index'])
-        ->name('report.index');
+    Route::get('/reports', [ReportDetailController::class, 'index'])
+        ->name('reports.index');
 });
