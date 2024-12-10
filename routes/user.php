b352721b4306900a58c1fe37e99c0b237c9bbf4d
Harry-Yeh <harryyeh428@gmail.com>
2024-12-10 19:21:03 +0800

    使用者後台 路徑合併(resource)

--- routes/user.php
+++ routes/user.php
@@ -7,29 +7,12 @@ use App\Http\Controllers\User\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('user')->name('user.')->middleware(['auth', 'verified'])->group(function () {
-    // 我的商品頁面
-    Route::get('/products', [ProductController::class, 'index'])
-        ->name('products.index');
+    // 我的商品頁,刊登頁,建立商品,編輯頁,更新商品,刪除
+    Route::resource('products', ProductController::class)
+        ->except(['show']);
     // 商品上下架
     Route::put('/products/{product}/demote', [ProductController::class, 'demoteData'])
         ->name('products.demoteData');
-    // 商品修改頁面
-    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])
-        ->name('products.edit');
-    // 商品>修改 資料
-    Route::put('/products/{product}', [ProductController::class, 'update'])
-        ->name('products.update');
-
-    // 商品刪除
-    Route::delete('/products/{product}', [ProductController::class, 'destroy'])
-        ->name('products.destroy');
-
-    // 刊登商品頁面
-    Route::get('/products/create', [ProductController::class, 'create'])
-        ->name('products.create');
-    // 商品建立資料
-    Route::post('/products', [ProductController::class, 'store'])
-        ->name('products.store');
 
     // 留言 建立,編輯頁,更新留言,刪除
     Route::resource('products.messages', MessageController::class)
