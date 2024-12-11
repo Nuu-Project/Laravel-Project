3803e5dca7c10879b92fef3bce40936943a5ff4e
Harry-Yeh <harryyeh428@gmail.com>
2024-12-11 19:26:45 +0800

    管理者後台 檢舉詳情 搜尋修改

--- app/Http/Controllers/Admin/ReportDetailController.php
+++ app/Http/Controllers/Admin/ReportDetailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Reportable;
+use Illuminate\Database\Eloquent\Builder;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
@@ -11,13 +12,20 @@ class ReportDetailController extends Controller
 {
     public function index()
     {
-
         $reportables = QueryBuilder::for(Reportable::class)
             ->allowedFilters([
-                AllowedFilter::exact('reportable_id'),
+                AllowedFilter::callback('reportable_id', function (Builder $query, $value) {
+                    $query->Where('reportable_id', $value);
+                }),
+                AllowedFilter::callback('name', function (Builder $query, $value) {
+                    $query->WhereHasMorph('reportable', ['App\Models\Product'], function ($query) use ($value) {
+                        $query->where('name', 'like', "%{$value}%");
+                    });
+                }),
             ])
-            ->with('report', 'reportable', 'whistleblower')
-            ->paginate(5);
+            ->with(['reportable'])
+            ->paginate(10)
+            ->withQueryString();
 
         return view('admin.report', compact('reportables'));
     }
