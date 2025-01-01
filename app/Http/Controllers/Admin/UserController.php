29d14e8eecd557d28df5521d788462ccf5569016
Harry-Yeh <harryyeh428@gmail.com>
2025-01-01 23:32:22 +0800

    用戶暫停取時間方式修改

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use DateTime;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Spatie\QueryBuilder\AllowedFilter;
@@ -30,20 +29,14 @@ class UserController extends Controller
         return view('admin.user', compact('users'));
     }
 
-    public function suspend(Request $request)
+    public function suspend(Request $request, User $user)
     {
         $validated = $request->validate([
-            'user_id' => 'required|exists:users,id',
-            'duration' => 'required|integer',
-            'reason' => 'nullable|string|max:255',
+            'duration' => ['required', 'integer', 'min:0'],
+            'reason' => ['nullable', 'string', 'max:255'],
         ]);
 
-        $duration = (int) $request->input('duration');
-        $suspendUntil = new DateTime;
-        $suspendUntil->modify("+{$duration} seconds");
-
-        $user = User::find($request->input('user_id'));
-        $user->time_limit = $suspendUntil;
+        $user->time_limit = now()->addSeconds($request->integer('duration'));
         $user->save();
 
         return response()->json(['message' => '用戶已成功暫停']);
