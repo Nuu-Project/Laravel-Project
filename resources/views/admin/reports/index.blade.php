f7f0aa4cf54f9bdbe9deeb0262a33ef855c236c0
kinyao <kinyao1025@gmail.com>
2025-01-02 20:57:10 +0800

    placeholder內文修正&圖片保留

--- resources/views/admin/reports/index.blade.php
+++ resources/views/admin/reports/index.blade.php
@@ -13,7 +13,7 @@
                         <div>
                             <input type="text" id="filter[name]" name="filter[name]"
                                 class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="請輸入查詢商品名稱..." value="{{ request('filter.name') }}">
+                                placeholder="請輸入商品名稱..." value="{{ request('filter.name') }}">
                             <x-button.search>
                                 搜索
                             </x-button.search>
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -14,7 +14,7 @@
                         <div>
                             <input type="text" name="filter[name]" id="filter[name]"
                                 class="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                                placeholder="Search..." value="{{ request('filter.name') ?? '' }}">
+                                placeholder="請輸入用戶名稱..." value="{{ request('filter.name') ?? '' }}">
                             <x-button.search>
                                 搜索
                             </x-button.search>
