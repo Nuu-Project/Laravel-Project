6b21b203b5bebf3cc9915c818a522a5cdd704ce0
kinyao <kinyao1025@gmail.com>
2025-01-12 18:16:24 +0800

    編輯頁面金額欄位格式&標籤頁面新增判斷

--- resources/views/user/products/edit.blade.php
+++ resources/views/user/products/edit.blade.php
@@ -45,12 +45,12 @@
                         <label
                             class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                             for="price">
-                            價格
+                            價格(不可修改)
                         </label>
                         <input
                             class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                             id="price" name="price" placeholder="輸入價格" type="number"
-                            value="{{ $product->price }}" disabled readonly />
+                            value="{{ $product->price }}" readonly />
                         <x-input-error :messages="$errors->get('price')" class="mt-2" />
                     </div>
 
