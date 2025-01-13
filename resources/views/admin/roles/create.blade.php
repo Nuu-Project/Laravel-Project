832490230e2092c2966ca910f6d8cb65e36421c0
Walen <weilinchiang24@gmail.com>
2025-01-13 19:40:31 +0800

    Admin-roles-create元件更新

--- resources/views/admin/roles/create.blade.php
+++ resources/views/admin/roles/create.blade.php
@@ -20,58 +20,56 @@
                     </form>
                 </div>
 
-            <div class="bg-white rounded-lg shadow-md p-6">
-                <!-- 表單開始 -->
-                <form action="{{ route('admin.roles.store') }}" method="POST">
-                    @csrf <!-- CSRF 保護 -->
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <x-thead.roles />
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @foreach ($users as $user)
-                                @if (!$user->hasRole('admin') && !$user->hasRole('user'))
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <!-- 勾選框，傳遞每個用戶的ID -->
-                                            <input type="checkbox" name="user_ids[]" value="{{ $user->id }}"
-                                                class="form-checkbox h-4 w-4 text-blue-600">
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                                            {{ $user->name }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                            {{ $user->email }}
-                                        </td>
-                                    </tr>
-                                @endif
-                            @endforeach
-                        </tbody>
-                    </table>
+                <div class="bg-white rounded-lg shadow-md p-6">
+                    <!-- 表單開始 -->
+                    <form action="{{ route('admin.roles.store') }}" method="POST">
+                        @csrf <!-- CSRF 保護 -->
+                        <table class="min-w-full divide-y divide-gray-200">
+                            <x-thead.roles />
+                            <tbody class="bg-white divide-y divide-gray-200">
+                                @foreach ($users as $user)
+                                    @if (!$user->hasRole('admin') && !$user->hasRole('user'))
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap">
+                                                <!-- 勾選框，傳遞每個用戶的ID -->
+                                                <input type="checkbox" name="user_ids[]" value="{{ $user->id }}"
+                                                    class="form-checkbox h-4 w-4 text-blue-600">
+                                            </td>
+                                            <x-td.gray-900>
+                                                {{ $user->name }}
+                                            </x-td.gray-900>
+                                            <x-td.gray-500>
+                                                {{ $user->email }}
+                                            </x-td.gray-500>
+                                        </tr>
+                                    @endif
+                                @endforeach
+                            </tbody>
+                        </table>
 
-                    <!-- 分頁 -->
-                    <div>
-                        {{ $users->links() }}
-                    </div>
+                        <!-- 分頁 -->
+                        <div>
+                            {{ $users->links() }}
+                        </div>
 
-                    <!-- 提交按鈕 -->
-                    <div class="mt-4 flex justify-end space-x-4">
-                        <button type="button" id="cancelBtn"
-                            class="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                            取消
-                        </button>
-                        <button type="submit"
-                            class="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                            確認
-                        </button>
-                    </div>
-                </form>
+                        <!-- 提交按鈕 -->
+                        <div class="mt-4 flex justify-end space-x-4">
+                            <x-button.roles type="button" id="cancelBtn">
+                                取消
+                            </x-button.roles>
+                            <x-button.roles type="submit">
+                                確認
+                            </x-button.roles>
+                        </div>
+                    </form>
+                </div>
+
+                <script>
+                    // 取消按鈕
+                    document.getElementById('cancelBtn').addEventListener('click', function() {
+                        window.location.href = '{{ route('admin.roles.index') }}';
+                    });
+                </script>
             </div>
-
-            <script>
-                // 取消按鈕
-                document.getElementById('cancelBtn').addEventListener('click', function() {
-                    window.location.href = '{{ route('admin.roles.index') }}';
-                });
-            </script>
-        </div>
     </main>
 </x-template-admin-layout>
