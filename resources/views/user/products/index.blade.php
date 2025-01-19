a401a82a1e3635681d55b517f4c1ca8466b3893f
kinyao <kinyao1025@gmail.com>
2025-01-19 21:37:10 +0800

    商品介紹50個字&用戶管理搜尋框

--- resources/views/user/products/index.blade.php
+++ resources/views/user/products/index.blade.php
@@ -36,11 +36,6 @@
 
             <div class="flex flex-col w-full min-h-screen">
                 <main class="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
-                    {{-- @if ($message)
-                        <div class="alert alert-info text-lg font-semibold text-center text-blue-500 p-4">
-                            {{ $message }}
-                        </div>
-                    @endif --}}
                     <div class="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
                         @foreach ($userProducts as $product)
                             <div class="rounded-lg border bg-white text-card-foreground shadow-sm p-6" data-v0-t="card">
@@ -98,8 +93,7 @@
                                     </div>
                                 </div>
                                 <div class="flex justify-center space-x-4 mt-6">
-                                    <form action="{{ route('user.products.inactive', $product->id) }}"
-                                        method="POST">
+                                    <form action="{{ route('user.products.inactive', $product->id) }}" method="POST">
                                         @csrf
                                         @method('PUT')
                                         <input type="hidden" name="product_id" value="{{ $product->id }}">
