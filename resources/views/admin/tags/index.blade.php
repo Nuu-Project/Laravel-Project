2cc362598dbbaedf8d17b5f76bd59f9095adda1c
kinyao <kinyao1025@gmail.com>
2025-01-23 21:34:22 +0800

    搜尋欄放進conpoment

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -2,11 +2,10 @@
 
     <!-- 主要內容 -->
     <x-main.flex-container>
-        <div class="max-w-7xl mx-auto">
+        <x-div.container>
             <x-h.h3>標籤管理</x-h.h3>
             <div class="p-4">
-                <a href="{{ route('admin.tags.create') }}"><button
-                        class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">新增標籤</button></a>
+                <a href="{{ route('admin.tags.create') }}"> <x-button.search>新增標籤</x-button.search></a>
             </div>
             <div class="bg-white rounded-lg shadow">
                 <div class="overflow-x-auto">
@@ -18,6 +17,6 @@
                     </x-div.gray-200>
                 </div>
             </div>
-        </div>
+        </x-div.container>
     </x-main.flex-container>
 </x-template-admin-layout>
