3d22147a5e12863150f56740055a00ed47d1c9fd
kinyao <kinyao1025@gmail.com>
2025-01-23 21:07:02 +0800

    搜尋賣家&刪除無用註解

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -17,13 +17,4 @@
         <div class="max-w-xl">
             @include('profile.update-password-form')
         </div>
-    </div>
-
-    {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-        <div class="max-w-xl">
-            @include('profile.delete-user-form')
-        </div> --}}
-    </div>
-    </div>
-    </div>
 </x-template-layout>
