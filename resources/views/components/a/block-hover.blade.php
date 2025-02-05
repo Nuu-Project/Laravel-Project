746ed7eaf00d2b6ad592394f2e22c978356bce14
Walen <weilinchiang24@gmail.com>
2025-02-06 00:21:39 +0800

    side-bar-admin & user中的 <a> 元件更新

---
+++ resources/views/components/a/block-hover.blade.php
@@ -0,0 +1,4 @@
+<a
+    {{ $attributes->merge(['type' => '', 'class' => 'block py-2 px-4 text-gray-700 hover:bg-gray-200']) }}>
+    {{ $slot }}
+</a>
--- resources/views/components/navbar/side-bar-user.blade.php
+++ resources/views/components/navbar/side-bar-user.blade.php
@@ -7,10 +7,10 @@
         使用者後台
     </div>
     <nav class="mt-4" x-data="{ open: true }">
-        <div @click="open = !open" class="block py-2 px-4 text-gray-700 hover:bg-gray-200 cursor-pointer">
+        <div @click="open = !open" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">
             <div class="flex justify-between items-center">
                 <span>商品管理系統</span>
-                <svg :class="{ 'transfo  rm rotate-180': open }" class="w-4 h-4 transition-transform duration-200"
+                <svg :class="{ 'transform rotate-180': open }" class="w-4 h-4 transition-transform duration-200"
                     fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd"
                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -19,10 +19,8 @@
             </div>
         </div>
         <div x-show="open" class="pl-4">
-            <a href="{{ route('user.products.index') }}"
-                class="block py-2 px-4 text-gray-700 hover:bg-gray-200">我的商品</a>
-            <a href="{{ route('user.products.create') }}"
-                class="block py-2 px-4 text-gray-700 hover:bg-gray-200">刊登商品</a>
+            <x-a.block-hover href="{{ route('user.products.index') }}">我的商品</x-a.block-hover>
+            <x-a.block-hover href="{{ route('user.products.create') }}">刊登商品</x-a.block-hover>
         </div>
     </nav>
 </div>
