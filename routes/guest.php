fde6cf63337b677f83c7fdb7aaa741630f0ab2d5
Harry-Yeh <harryyeh428@gmail.com>
2025-02-06 11:55:58 +0800

    route web 將 guest 分離

---
+++ routes/guest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Guest\HomeController;
+use App\Http\Controllers\Guest\ProductController;
+use Illuminate\Support\Facades\Route;
+
+    // 首頁
+    Route::get('/', [HomeController::class, 'index'])
+        ->name('dashboard');
+
+    // 商品 瀏覽頁,洽談頁
+    Route::resource('products', ProductController::class)
+        ->only(['index', 'show']);
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-use App\Http\Controllers\Guest\HomeController;
-use App\Http\Controllers\Guest\ProductController;
-use Illuminate\Support\Facades\Route;
-
-// 首頁
-Route::get('/', [HomeController::class, 'index'])
-    ->name('dashboard');
-
-// 商品 瀏覽頁,洽談頁
-Route::resource('products', ProductController::class)
-    ->only(['index', 'show']);
-
+require __DIR__.'/guest.php';
 require __DIR__.'/auth.php';
-require __DIR__.'/admin.php';
 require __DIR__.'/user.php';
+require __DIR__.'/admin.php';
 require __DIR__.'/api/user.php';
