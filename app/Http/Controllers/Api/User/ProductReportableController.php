a457021c55934b6afb0d792fea55a1c5f68ca26d
Harry-Yeh <harryyeh428@gmail.com>
2025-02-10 23:55:57 +0800

    controller 使用者檢舉 驗證&創建修正

--- app/Http/Controllers/Api/User/ProductReportableController.php
+++ app/Http/Controllers/Api/User/ProductReportableController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\Report;
 use App\Models\Reportable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
 class ProductReportableController extends Controller
@@ -17,20 +19,32 @@ class ProductReportableController extends Controller
             'report_type_id' => [
                 'required',
                 'exists:report_types,id',
-                Rule::unique('reportables')
-                    ->where('reportable_id', $product->id)
-                    ->where('reportable_type', Product::class)
-                    ->where('user_id', Auth::id()),
+                Rule::unique('reports')
+                    ->where('report_type_id', $request->input('report_type_id'))
+                    ->where('user_id', Auth::id())
+                    ->where(function ($query) use ($product) {
+                        return $query->whereExists(function ($query) use ($product) {
+                            $query->select(DB::raw(1))
+                                ->from('reportables')
+                                ->whereColumn('reportables.report_id', 'reports.id')
+                                ->where('reportable_id', $product->id)
+                                ->where('reportable_type', Product::class);
+                        });
+                    }),
             ],
             'description' => 'required|string|max:255',
         ]);
 
-        Reportable::create([
+        $report = Report::create([
             'report_type_id' => $request->input('report_type_id'),
-            'reportable_id' => $product->id, // 關聯的 Product ID
-            'reportable_type' => Product::class, // 關聯的模型類型
             'user_id' => Auth::id(),
             'description' => $request->input('description'),
+        ]);
+
+        Reportable::create([
+            'report_id' => $report->id,
+            'reportable_id' => $product->id,
+            'reportable_type' => Product::class,
         ]);
 
         return response()->json(['status' => 'success']);
