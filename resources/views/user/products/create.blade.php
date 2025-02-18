20ef21bdc9e1e5bec811d97cbd4e93fe9586f460
kinyao <kinyao1025@gmail.com>
2025-02-18 20:44:47 +0800

    測試

--- resources/views/user/products/create.blade.php
+++ resources/views/user/products/create.blade.php
@@ -212,7 +212,7 @@
 
                     const result = await response.json();
 
-                    if (result.success) {
+                    if (result.success) {測試
                         processedImagePaths[number] = result.path;
 
                         const reader = new FileReader();
