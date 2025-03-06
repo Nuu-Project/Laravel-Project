cad74dcca4c8f72af698f82a1520873d9eadd335
Harry-Yeh <harryyeh428@gmail.com>
2025-03-06 17:50:03 +0800

    管理者 檢舉類型 create頁面修改

--- resources/views/admin/report_types/create.blade.php
+++ resources/views/admin/report_types/create.blade.php
@@ -6,30 +6,29 @@
             <x-h.h3>新增檢舉類型</x-h.h3>
 
             <!-- 表單內容 -->
-            <form id="reportCategoryForm" class="mt-8 space-y-6">
+            <form id="reportCategoryForm" class="mt-8 space-y-6" action="{{ route('admin.report_types.store') }}"
+                method="POST">
+                @csrf
 
                 <x-div.grid>
-                    <x-tags for="name">Name:</x-tags>
+                    <x-tags for="name[zh_TW]">Name:</x-tags>
                     <x-input.tags id="name" name="name" placeholder="輸入新的中文 name" required>
                     </x-input.tags>
-                </x-div.grid>
-
-                <x-div.grid>
-                    <x-tags for="slug[zh_TW]">Slug:</x-tags>
-                    <x-input.tags id="slug" name="slug" placeholder="輸入新的中文 slug" required>
-                    </x-input.tags>
+                    <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </x-div.grid>
 
                 <x-div.grid>
                     <x-tags for="type">type:</x-tags>
                     <x-input.tags id="type" name="type" placeholder="輸入新的中文 type" required>
                     </x-input.tags>
+                    <x-input-error :messages="$errors->get('type')" class="mt-2" />
                 </x-div.grid>
 
                 <x-div.grid>
                     <x-tags for="order_column">Order_Column:</x-tags>
                     <x-input.tags id="order_column" name="order_column" placeholder="輸入新的Order Column" required>
                     </x-input.tags>
+                    <x-input-error :messages="$errors->get('order_column')" class="mt-2" />
                 </x-div.grid>
 
                 <x-button.create-edit>
