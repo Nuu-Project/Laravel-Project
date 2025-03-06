689b21b7abbd9eae7077ffeaf1d40657ba179cd5
yancheng <dppss99015@gmail.com>
2025-03-06 16:33:33 +0800

    reportables改report

--- routes/api/message.php
+++ routes/api/message.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\Api\User\MessageReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::post('messages/{message}/reportables', [MessageReportController::class, 'store'])
-        ->name('messages.reportables.store');
+    Route::post('messages/{message}/report', [MessageReportController::class, 'store'])
+        ->name('messages.report.store');
 });
