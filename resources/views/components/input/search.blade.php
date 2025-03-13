7bcdc8d7ce8f4c4d44314e1fb674cbf2e6c9e72c
Walen <weilinchiang24@gmail.com>
2025-03-13 18:56:29 +0800

    搜尋欄位 元件修正

--- resources/views/components/input/search.blade.php
+++ resources/views/components/input/search.blade.php
@@ -1,4 +1,4 @@
 <input
-    {{ $attributes->merge(['type' => 'text', 'class' => 'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50']) }}>
+    {{ $attributes->merge(['type' => 'text', 'class' => 'px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500']) }}>
     {{ $slot }}
 </input>
