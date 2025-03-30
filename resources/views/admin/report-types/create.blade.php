9de4b0f5eab1a1fd788526aee5b4912dc5985a1b
kinyao <kinyao1025@gmail.com>
2025-03-30 16:14:51 +0800

    admin 註解刪除

--- resources/views/admin/report-types/create.blade.php
+++ resources/views/admin/report-types/create.blade.php
@@ -1,11 +1,9 @@
 <x-template-admin-layout>
 
-    <!-- 主要內容 -->
     <x-flex-container>
         <x-div.container>
             <x-h.h3>新增檢舉類型</x-h.h3>
 
-            <!-- 表單內容 -->
             <form id="reportCategoryForm" class="mt-8 space-y-6" action="{{ route('admin.report-types.store') }}"
                 method="POST">
                 @csrf
--- resources/views/admin/tags/edit.blade.php
+++ resources/views/admin/tags/edit.blade.php
@@ -1,17 +1,14 @@
 <x-template-admin-layout>
 
-    <!-- 主要內容 -->
     <x-flex-container>
         <x-div.container>
             <x-h.h3>編輯標籤</x-h.h3>
 
-            <!-- 這裡放置原有的表單內容 -->
             <form class="mt-8 space-y-6" action="{{ route('admin.tags.update', $tag->id) }}" method="POST"
                 enctype="multipart/form-data">
                 @csrf
-                @method('PUT') <!-- 用於更新資料 -->
+                @method('PUT')
 
-                <!-- Name 欄位 -->
                 <x-div.grid>
                     <x-tags for="name">Name:</x-tags>
                     <x-input.tags id="name" name="name" placeholder="請輸入修改後的Name"
@@ -19,7 +16,6 @@
                     <x-input-error :messages="$errors->get('name')" class="mt-2" />
                 </x-div.grid>
 
-                <!-- Slug 欄位 -->
                 <x-div.grid>
                     <x-tags for="slug">Slug:</x-tags>
                     <x-input.tags id="slug" name="slug" placeholder="輸入修改後的Slug"
@@ -27,7 +23,6 @@
                     <x-input-error :messages="$errors->get('slug')" class="mt-2" />
                 </x-div.grid>
 
-                <!-- Type 欄位 -->
                 <x-div.grid>
                     <x-tags for="type">Type:</x-tags>
                     <x-input.tags id="type" name="type" placeholder="輸入修改後的Type"
@@ -35,7 +30,6 @@
                     <x-input-error :messages="$errors->get('type')" class="mt-2" />
                 </x-div.grid>
 
-                <!-- Order_column 欄位 -->
                 <x-div.grid>
                     <x-tags for="order_column">Order Column:</x-tags>
                     <x-input.tags id="order_column" name="order_column" placeholder="輸入修改後的Order Column"
@@ -43,7 +37,6 @@
                     <x-input-error :messages="$errors->get('order_column')" class="mt-2" />
                 </x-div.grid>
 
-                <!-- 提交按鈕 -->
                 <x-button.create-edit>
                     確定修改標籤
                 </x-button.create-edit>
