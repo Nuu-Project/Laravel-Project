87bb8cbb81a94e3d9773be745733e0008ef28a5b
Walen <weilinchiang24@gmail.com>
2025-04-01 03:45:21 +0800

    管理者 項目分類

--- resources/views/components/navbar/side-bar-admin.blade.php
+++ resources/views/components/navbar/side-bar-admin.blade.php
@@ -8,7 +8,7 @@
     </div>
     <nav class="mt-4" x-data="{ open: true }">
         <div @click="open = !open" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">
-            <div class="flex justify-between items-center">
+            <x-div.side-bar-admin>
                 <span>後台管理系統</span>
                 <svg :class="{ 'transform rotate-180': open }" class="w-4 h-4 transition-transform duration-200"
                     fill="currentColor" viewBox="0 0 20 20">
@@ -16,7 +16,7 @@
                         d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
                         clip-rule="evenodd"></path>
                 </svg>
-            </div>
+            </x-div.side-bar-admin>
         </div>
         <div x-show="open" class="pl-4">
             <x-a.block-hover href="{{ route('admin.products.index') }}">商品管理</x-a.block-hover>
@@ -24,6 +24,22 @@
             <x-a.block-hover href="{{ route('admin.messages.index') }}">留言管理</x-a.block-hover>
             <x-a.block-hover href="{{ route('admin.roles.index') }}">角色管理</x-a.block-hover>
             <x-a.block-hover href="{{ route('admin.reports.index') }}">檢舉管理</x-a.block-hover>
+        </div>
+    </nav>
+
+    <nav class="mt-4" x-data="{ open: true }">
+        <div @click="open = !open" class="block py-2 px-4 text-gray-700 hover:bg-gray-200">
+            <x-div.side-bar-admin>
+                <span>標籤與檢舉類型管理系統</span>
+                <svg :class="{ 'transform rotate-180': open }" class="w-4 h-4 transition-transform duration-200"
+                    fill="currentColor" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd"
+                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </x-div.side-bar-admin>
+        </div>
+        <div x-show="open" class="pl-4">
             <x-a.block-hover href="{{ route('admin.tags.index') }}">新增與停用標籤</x-a.block-hover>
             <x-a.block-hover href="{{ route('admin.report-types.index') }}">新增與停用檢舉類型</x-a.block-hover>
         </div>
--- resources/views/guest/products/show.blade.php
+++ resources/views/guest/products/show.blade.php
@@ -87,7 +87,7 @@
                             d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
                     </svg>
                     <div class="flex-1">
-                        <div class="flex justify-between items-center">
+                        <x-div.side-bar-admin>
                             <span class="text-gray-800">{{ $message->user->name }}</span>
                             <div class="flex items-center">
                                 <small
@@ -151,7 +151,7 @@
                                     </x-dropdown>
                                 @endif
                             </div>
-                        </div>
+                        </x-div.side-bar-admin>
                         <p class="mt-4 text-lg text-gray-900 whitespace-pre-line">{{ $message->message }}</p>
 
                         <!-- 回覆按鈕 -->
@@ -173,7 +173,7 @@
                         <!-- 顯示回覆的留言 -->
                         @foreach ($message->replies as $reply)
                             <div class="ml-8 mt-4 p-4 bg-gray-50 rounded-lg">
-                                <div class="flex justify-between items-center">
+                                <x-div.side-bar-admin>
                                     <span class="text-gray-800">{{ $reply->user->name }}</span>
                                     <div class="flex items-center">
                                         <small
@@ -240,7 +240,7 @@
                                             </x-dropdown>
                                         @endif
                                     </div>
-                                </div>
+                                </x-div.side-bar-admin>
                                 <p class="mt-2 text-gray-900 whitespace-pre-line">{{ $reply->message }}</p>
                             </div>
                         @endforeach
