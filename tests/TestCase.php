3958dd220b1d296f1ce78029920160683c55581c
yancheng <dppss99015@gmail.com>
2025-04-12 15:12:41 +0800

    TestCase設定

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+    }
+
+    public function createUser(): User
+    {
+        return User::factory()->create();
+    }
+
+    public function actingAsUser(): void
+    {
+        $this->actingAs($this->createUser());
+    }
+
+    public function logout(): void
+    {
+        $this->post('/logout');
+
+        $this->assertGuest();
+    }
 }
