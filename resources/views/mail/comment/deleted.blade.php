e7f52f73754d2f5ac91a8ce2fc939262079be75f
kinyao <kinyao1025@gmail.com>
2025-04-17 21:32:57 +0800

    留言刪除通知信排版

--- resources/views/mail/comment/deleted.blade.php
+++ resources/views/mail/comment/deleted.blade.php
@@ -1,22 +1,32 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>留言已刪除通知</title>
-</head>
-<body>
-    <p>親愛的 {{ $name ?? '使用者' }} 您好，</p>
+<x-mail::message>
+{{"留言刪除通知"}}
 
-    <p>我們很抱歉地通知您，您在我們平台上的留言已被管理員刪除。</p>
+請注意：此信件由系統自動發送，請勿直接回覆此信
 
-    @if ($message)
-        <p><strong>被刪除的留言內容：</strong></p>
-        <p>{{ $message }}</p>
-    @endif
+親愛的 {{ $name ?? '使用者' }}：
 
-    <p>如果您對此有任何疑問，請聯繫我們的客服團隊。</p>
+我們很抱歉地通知您，您在我們的書網上的留言已被管理員刪除。
 
-    <p>感謝您的理解與合作。</p>
+{{-- @if ($user && $user->suspend_reason)
+    原因：{{ $user->suspend_reason }}<br>
+@endif --}}
 
-    <p><strong>{{ config('app.name') }}</strong></p>
-</body>
-</html>
+@if ($message)
+<x-mail::panel>
+{{-- 使用 panel 元件來突顯被刪除的內容 --}}
+<strong>被刪除的留言內容：</strong><br>
+{{ $message }}
+</x-mail::panel>
+@endif
+
+<x-mail::button :url="config('app.url')">
+返回書網
+</x-mail::button>
+
+感謝您的理解與合作。<br>
+{{ config('app.name') }} 團隊
+
+<small style="color: #718096;">
+此信件由系統自動發送，請勿直接回覆
+</small>
+</x-mail::message>
