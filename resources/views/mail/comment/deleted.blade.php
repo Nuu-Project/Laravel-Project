8dbbd89058e9125c73e4c0c91a2b7bf9784e1831
kinyao <kinyao1025@gmail.com>
2025-04-17 21:51:43 +0800

    通知信內文更新

--- resources/views/mail/comment/deleted.blade.php
+++ resources/views/mail/comment/deleted.blade.php
@@ -5,15 +5,10 @@
 
 親愛的 {{ $name ?? '使用者' }}：
 
-我們很抱歉地通知您，您在我們的書網上的留言已被管理員刪除。
-
-{{-- @if ($user && $user->suspend_reason)
-    原因：{{ $user->suspend_reason }}<br>
-@endif --}}
+我們很抱歉地通知您違反了我們的使用條款，因此您在書網上的留言已被管理員刪除。
 
 @if ($message)
 <x-mail::panel>
-{{-- 使用 panel 元件來突顯被刪除的內容 --}}
 <strong>被刪除的留言內容：</strong><br>
 {{ $message }}
 </x-mail::panel>
@@ -23,8 +18,8 @@
 返回書網
 </x-mail::button>
 
-感謝您的理解與合作。<br>
-{{ config('app.name') }} 團隊
+感謝您的理解與合作<br>
+聯大二手書網
 
 <small style="color: #718096;">
 此信件由系統自動發送，請勿直接回覆
