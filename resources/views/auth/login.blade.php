69af38efa45eae8dc630fcdf444f7845c428a22b
kinyao <kinyao1025@gmail.com>
2025-04-17 13:29:40 +0800

    登入介面修改+localhost回應 修改成正式回應

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,4 @@
 <x-template-login-register-layout>
-
-    <!-- home section -->
-    <section class="bg-white py-10 md:mb-10">
-        <x-div.container-screen>
-            <nav class="flex-wrap lg:flex items-center" x-data="{ navbarOpen: false }">
-                <div class="flex items-center mb-10 lg:mb-0">
-                    <img src="images/book-4-fix.png" alt="Logo">
-                    <button
-                        class="lg:hidden w-10 h-10 ml-auto flex items-center justify-center border border-blue-500 text-blue-500 rounded-md"
-                        @click="navbarOpen = !navbarOpen">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-menu">
-                            <line x1="3" y1="12" x2="21" y2="12"></line>
-                            <line x1="3" y1="6" x2="21" y2="6"></line>
-                            <line x1="3" y1="18" x2="21" y2="18"></line>
-                        </svg>
-                    </button>
-                </div>
-                <ul class="lg:flex flex-col lg:flex-row lg:items-center lg:mx-auto lg:space-x-8 xl:space-x-14"
-                    :class="{ 'hidden': !navbarOpen, 'flex': navbarOpen }">
-                    <x-li.font-semibold><a href="{{ route('dashboard') }}">首頁</a></x-li.font-semibold>
-                    <x-li.font-semibold><a href="{{ route('products.index') }}">商品</a></x-li.font-semibold>
-                </ul>
-                <div class="lg:flex flex-col md:flex-row md:items-center text-center md:space-x-6"
-                    :class="{ 'hidden': !navbarOpen, 'flex': navbarOpen }">
-                    @if (Route::has('register'))
-                        <x-a.register href="/register">註冊</x-a.register>
-                    @endif
-                    @if (Route::has('login'))
-                        @auth
-                            <a href="{{ url('/dashboard') }}">Dashboard</a>
-                        @else
-                            <x-a.login href="/login">登入</x-a.login>
-                        @endif
-                    @endauth
-                </div>
-            </nav>
-        </x-div.container-screen>
-    </section>
-
     <x-guest-layout>
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
