d3c8ff676ed6ca30d1c04ecd887bae9a33e833d1
Walen <weilinchiang24@gmail.com>
2025-05-21 22:11:00 +0800

    登入/登出 RWD按鈕大小修正

--- resources/views/components/a/login.blade.php
+++ resources/views/components/a/login.blade.php
@@ -1,4 +1,4 @@
 <a
-    {{ $attributes->merge(['type' => '', 'class' => 'inline-block px-4 sm:px-6 py-3 sm:py-4 border-2 border-blue-500 text-blue-500 font-semibold text-base sm:text-lg rounded-xl hover:bg-blue-700 hover:text-white transition ease-linear duration-500 w-full md:w-auto text-center']) }}>
+    {{ $attributes->merge(['type' => '', 'class' => 'px-4 sm:px-6 py-3 sm:py-4 bg-blue-500 text-white font-semibold text-base sm:text-lg rounded-xl hover:bg-blue-700 transition ease-in-out duration-500 mb-5 md:mb-0 w-full md:w-auto text-center']) }}>
     {{ $slot }}
 </a>
