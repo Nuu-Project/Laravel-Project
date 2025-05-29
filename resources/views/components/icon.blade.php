0b875f7fdb23c0b869e916791ac50f8ee9fdd679
Walen <weilinchiang24@gmail.com>
2025-05-29 16:53:47 +0800

    首頁icon元件製作&route問題

---
+++ resources/views/components/icon.blade.php
@@ -0,0 +1,50 @@
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20">
+    <x-div.center>
+        <x-div.justify>
+            <x-div.opacity>
+                <i data-feather="globe"></i>
+            </x-div.opacity>
+        </x-div.justify>
+
+        <x-h.h4>簡單的操作介面</x-h.h4>
+
+        <x-p.font-black>沒有繁瑣的流程，一切步驟都經過精簡</x-p.font-black>
+
+        <x-div.justify>
+            </a>
+        </x-div.justify>
+    </x-div.center>
+
+    <x-div.center>
+        <x-div.justify>
+            <x-div.opacity>
+                <i data-feather="arrow-up-right"></i>
+            </x-div.opacity>
+        </x-div.justify>
+
+        <x-h.h4>專為聯大服務的平台</x-h.h4>
+
+        <x-p.font-black>在這裡刊登二手書的商品，都是可能或必須用到的書</x-p.font-black>
+
+        <x-div.justify>
+
+        </x-div.justify>
+    </x-div.center>
+
+    <x-div.center>
+        <x-div.justify>
+            <x-div.opacity>
+                <i data-feather="clock"></i>
+            </x-div.opacity>
+        </x-div.justify>
+
+        <x-h.h4>方便省時</x-h.h4>
+
+        <x-p.font-black>每個的人時間都很寶貴，我們想讓大家用最快的速度買到自己需要的教科書</x-p.font-black>
+
+        <x-div.justify>
+
+        </x-div.justify>
+    </x-div.center>
+
+</div>
