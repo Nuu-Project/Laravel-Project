9ed030734c15d64225e0755f86a907ec6704d819
Walen <weilinchiang24@gmail.com>
2025-06-26 16:32:55 +0800

    Guest=>index的標籤選單button改成元件

--- resources/views/guest/products/index.blade.php
+++ resources/views/guest/products/index.blade.php
@@ -74,19 +74,16 @@
                     </div>
                 </div>
 
-                <div class="flex justify-end mt-4 pt-3 border-t border-gray-200">
-                    <button type="button" id="clear-tag-selection"
-                        class="px-4 py-2 bg-gray-100 text-gray-700 rounded-md mr-2 hover:bg-gray-200 border border-gray-300">
+                <div>
+                    <x-button.clear>
                         清除所有
-                    </button>
-                    <button type="button" id="close-tag-selector"
-                        class="px-4 py-2 bg-gray-100 text-gray-700 rounded-md mr-2 hover:bg-gray-200 border border-gray-300">
+                    </x-button.clear>
+                    <x-button.close>
                         關閉
-                    </button>
-                    <button type="button" id="apply-tag-filters"
-                        class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                    </x-button.close>
+                    <x-button.apply>
                         查詢
-                    </button>
+                    </x-button.apply>
                 </div>
             </div>
 
