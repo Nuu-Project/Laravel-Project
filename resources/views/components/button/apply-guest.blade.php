07eeaeafceb016cae29a85b2ca0f59b420c5f40c
Walen <weilinchiang24@gmail.com>
2025-07-10 15:17:21 +0800

    重複class移除

---
+++ resources/views/components/button/apply-guest.blade.php
@@ -0,0 +1,4 @@
+<button
+    {{ $attributes->merge(['type' => 'submit', 'id' => 'apply-tag-filters', 'class' => 'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto']) }}>
+    {{ $slot }}
+</button>
