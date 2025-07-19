518f3f785c43629adbbc95a0a14788718d9134df
Walen <weilinchiang24@gmail.com>
2025-07-19 14:49:03 +0800

    apply改用Prop

--- resources/views/components/button/apply.blade.php
+++ resources/views/components/button/apply.blade.php
@@ -1,4 +1,12 @@
+@props([
+    'type' => 'button',
+])
+
 <button
-    {{ $attributes->merge(['type' => 'button', 'id' => 'apply-tag-filters', 'class' => 'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto']) }}>
+    {{ $attributes->merge([
+        'type' => $type,
+        'id' => 'apply-tag-filters',
+        'class' => 'bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded w-full sm:w-auto',
+    ]) }}>
     {{ $slot }}
 </button>
